43a8b33abf6cb85cb5dc46ffc5731a333e465ae9
Denise-juli <112222707+Denise-juli@users.noreply.github.com>
2023-11-26 22:48:20 -0300

    DAtos de las tablas

---
+++ EventosTestMVC/Migrations/20231127013917_AddTipoEventos.cs
@@ -0,0 +1,40 @@
+﻿using EventosTestMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System.Reflection.Emit;
+
+#nullable disable
+
+namespace EventosTestMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTipoEventos : Migration
+    {
+        /// <inheritdoc />
+       
+           protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "TipoEventos",
+                columns: new[] { "Id", "Nombre" },
+                values: new object[,]
+                {
+            { 1, "Cumpleaños" },
+            { 2, "Boda" },
+            { 3, "15 años" },
+            { 4, "Baby Shower" },
+            { 5, "Reunion con amigos" },
+            { 6, "Reunion familiar" },
+            { 7, "Despedida de soltera/o" },
+            { 8, "Piyamada" },
+            { 9, "Fiesta" }
+                });
+        }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
